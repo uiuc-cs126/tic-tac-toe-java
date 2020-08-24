54e40574a157deb738b130812d671cda9edfe27a
Elizabeth Wang <eywang1999@gmail.com>
2020-08-23 23:16:14 -0700

    Add javadoc comments to starter code

--- src/com/example/TicTacToeBoard.java
+++ src/com/example/TicTacToeBoard.java
@@ -1,11 +1,22 @@
 package com.example;
 
+/**
+ * Takes in and evaluates a string representing a tic tac toe board.
+ */
 public class TicTacToeBoard {
 
+  /**
+   * This method should load a string into your TicTacToeBoard class.
+   * @param board The string representing the board
+   */
   public TicTacToeBoard(String board) {
 
   }
 
+  /**
+   * Checks the state of hte board (unreachable, no winner, or winning)
+   * @return an enum value corresponding to the board evaluation
+   */
   public Evaluation evaluateBoard() {
     return Evaluation.UnreachableState;
   }
