82005e48e3150498a3f5a839d0b6e79edf1a28cf
Patrick J Gallagher <patrickj@cpgallagher.com>
2020-08-24 12:30:18 -0500

    Fix javadoc typo in Evaluation

--- src/com/example/Evaluation.java
+++ src/com/example/Evaluation.java
@@ -8,6 +8,6 @@ package com.example;
 public enum Evaluation {
   NoWinner, /* valid board, but the game hasn't been won by either player yet */
   Xwins, /* valid board, and the game was won by player X */
-  Owins, /* valid board, and the game was won by player Y */
+  Owins, /* valid board, and the game was won by player O */
   UnreachableState /* string describes a board, but not a board state that is reachable from a valid game */
 }
